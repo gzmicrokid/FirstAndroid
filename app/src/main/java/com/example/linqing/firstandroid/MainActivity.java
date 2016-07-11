2fbecbb8a35cea29bce6da39a187d03ab5375b35
gzmicrokid <l.dogdog@gmail.com>
2016-07-11 17:37:22 +0800

    test commit

--- app/src/main/java/com/example/linqing/firstandroid/MainActivity.java
+++ app/src/main/java/com/example/linqing/firstandroid/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.linqing.firstandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(
+                        MainActivity.this,
+                        R.string.btn_msg,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
